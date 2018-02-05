5ab01d594556c1da6051cffb6fbdd8d17bca313b
Dennis Aleynikov <daleynikov@gmail.com>
2018-02-05 10:55:42 -0800

    update slug

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 export default {
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'cosmic-js-chat',
+    slug: process.env.COSMIC_BUCKET | 'meme-chate',
     type_slug: 'messages'
   }
 }
